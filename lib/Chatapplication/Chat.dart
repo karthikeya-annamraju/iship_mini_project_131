6b184b9a2d8132200d64f98cf728f066a9d0d7f5
Rahul Varma Rudraraju <143200717+rahulvarma07@users.noreply.github.com>
2024-07-25 15:03:33 +0530

    --

--- lib/Chatapplication/Chat.dart
+++ lib/Chatapplication/Chat.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
-import 'package:chatapp/Chatapplication/chats.dart';
-import 'package:chatapp/Profile/profilepage.dart';
+import 'package:chat_app/Chatapplication/chats.dart';
+import 'package:chat_app/Profile/profilepage.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'Calls.dart';
